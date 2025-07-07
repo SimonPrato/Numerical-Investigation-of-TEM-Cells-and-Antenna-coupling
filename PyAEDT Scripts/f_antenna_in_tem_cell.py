57513b3e07429b53ce9ecea0271048e0dc1907af
Simon Prato <simip.prato@gmail.com>
2025-07-07 22:00:18 +0200

    Update f_antenna_in_tem_cell.py
    added comments in the script

--- PyAEDT Scripts/f_antenna_in_tem_cell.py
+++ PyAEDT Scripts/f_antenna_in_tem_cell.py
@@ -1,10 +1,25 @@
 from ansys.aedt.core.hfss import Hfss
 
+
 """
-This script inserts an inverted F antenna into a TEM cell in HFSS. 
-It uses the model of the TEM cell in the project "TEM_Cell_Project.aedt"
-provided by Dominik Kreindl.
+===============================================================================
+ Script Name   : f_antenna_in_tem_cell.py
+ Author        : Simon Prato
+ Created       : 2025-07-05
+ Last Modified : 2025-07-07
+ Version       : 1.0
+ Description   : This script inserts an inverted F antenna into a TEM cell in HFSS. 
+                 It uses the model of the TEM cell provided by Dominik Kreindl and
+                 of the inverted F-antenna by Texas Instruments.
+ Usage         : python f_antenna_in_tem_cell.py
+===============================================================================
+
+ Requirements  : Ansys Electronics Desktop must have PyAEDT 0.17.5 installed. 
+                 PyAEDT documentation: https://aedt.docs.pyansys.com/
+ Notes         : This script was written for a master thesis.
+===============================================================================
 """
+
 
 #  TODO: Add a start of simulation.
 #  TODO: Save all, including simulation results
@@ -12,20 +27,25 @@ provided by Dominik Kreindl.
 
 if __name__ == '__main__':
     tem_cell_project_name = "..\TEM Cell\Ansys Simulation Files\TEM_Cell_Project.aedt"
-    design_name = "TEM Cell with F Antenna"
     file_path_f_antenna = r"C:\Users\Simon Prato\Documents\Persoenliches\Studium\Masterarbeit\Inverted F Antenna\
                             Ansys Simulation Files\inverted_f_antenna_short_signal_conductor.a3dcomp"
+
+    design_name = "TEM Cell with F Antenna" # Name of the HFSS Design which will be created
 
     hfss = Hfss()
     hfss.load_project(file_name=tem_cell_project_name)
     hfss.duplicate_design(name=design_name, save_after_duplicate=True)
+    # Duplicate the original TEM cell design. This includes boundaries and the solution setup.
     modeler = hfss.modeler
-    # tem_cell = modeler.insert_3d_component(file_path_tem_cell)
     f_antenna = modeler.insert_3d_component(r"..\Inverted F Antenna\Ansys Simulation Files"
                                             r"\inverted_f_antenna_short_signal_conductor.a3dcomp")
-    print(f_antenna)
+    # Inserts the model of the inverted F-antenna
+    # Next, check if the antenna has been loaded correctly. If yes, then position it in the TEM cell.
     if f_antenna:
         f_antenna.rotate(axis="X", angle=270.0, units="deg")
         f_antenna.move([0, 0, 11.886672])
         hfss.save_project()
+    else:
+        print("ERROR: Antenna model not found.")
+
     hfss.release_desktop(close_projects=False, close_desktop=False)
