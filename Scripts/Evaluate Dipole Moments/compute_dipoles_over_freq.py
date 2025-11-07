ed762eeb487ad186abb8c451becdfde9700f9578
SimonPrato <simip.prato@gmail.com>
2025-11-07 16:04:24 +0100

    updated dipole computing script
    plot outputs are now of acceptable quality to put in documentation. The standard width is half an A4 page column.

--- Scripts/Evaluate Dipole Moments/compute_dipoles_over_freq.py
+++ Scripts/Evaluate Dipole Moments/compute_dipoles_over_freq.py
@@ -6,18 +6,19 @@ import matplotlib.pyplot as plt
 
 
 antenna_power = 1000000  # in Watt
-columns_phase_shift, columns_magnitude = read_csv(antenna_type="serial_loop")
+antenna_type = "monopole"
+columns_phase_shift, columns_magnitude = read_csv(antenna_type=antenna_type)
 frequencies = columns_phase_shift[0] * 1000000000 # Convert into Hz
-phase_shift = columns_phase_shift[1] - columns_phase_shift[2]
-# += np.pi
+phase_shift = columns_phase_shift[1] - columns_phase_shift[2] + np.pi
+
 magnitude = columns_magnitude[1]
 output_power = antenna_power * np.pow(10.0, magnitude/10)
 e_field_approx = np.sqrt(output_power) / 0.0012163
 
-plot_phase_shift(columns_phase_shift, frequencies)
+plot_phase_shift(columns_phase_shift, frequencies, antenna_type)
 
 m_e, m_m = calculate_moment(e_field_approx, phase_shift, output_power, frequencies)
-plot_moments(m_e, m_m, frequencies)
+plot_moments(m_e, m_m, frequencies, antenna_type)
 
 plot_output_power_e_field(frequencies, output_power, e_field_approx)
 
--- Scripts/Evaluate Dipole Moments/modules/calculate_moments.py
+++ Scripts/Evaluate Dipole Moments/modules/calculate_moments.py
@@ -5,9 +5,6 @@ def calculate_moment(e_field, phase_shift, output_power, frequency):
 
     a = output_power * 2
     b = output_power * 2 * np.exp(1j*phase_shift)
-    print(np.abs(a[-1]+b[-1]))
-    print(np.abs(a[-1]-b[-1]))
-    print(np.rad2deg(phase_shift[-1]))
 
     c = 299792458
     l = c / frequency
@@ -17,6 +14,8 @@ def calculate_moment(e_field, phase_shift, output_power, frequency):
 
     m_my = 1j*(a-b) / (e_field * k)
     m_hfss = 1j * m_my * 2 * np.pi * frequency * 1.256637 * np.pow(10.0,-6)
+
+    print(np.max(np.abs(m_hfss)))
 
     return np.abs(m_ez), np.abs(m_hfss)
 
