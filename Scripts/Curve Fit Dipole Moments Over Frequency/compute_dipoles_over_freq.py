bc2069c369e2e000bfae2c7cb85bd96226798f36
SimonPrato <simip.prato@gmail.com>
2025-11-28 13:36:45 +0100

    Added Script
    This script find parameters for a curve of order 3, which approximates the frequency behavior of the dipole moments

---
+++ Scripts/Curve Fit Dipole Moments Over Frequency/compute_dipoles_over_freq.py
@@ -0,0 +1,43 @@
+from scipy.optimize import curve_fit
+from modules.read_csv import *
+from modules.calculate_moments import *
+from modules.plot_moments import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+antenna_power = 1000000  # in Watt
+antenna_type = "small loop"
+columns_phase_shift, columns_magnitude = read_csv(antenna_type=antenna_type)
+frequencies = columns_phase_shift[0] * 1000000000 # Convert into Hz
+phase_shift = columns_phase_shift[1] - columns_phase_shift[2] + np.pi
+
+magnitude = columns_magnitude[1]
+output_power = antenna_power * np.pow(10.0, magnitude/10)
+e_field_approx = np.sqrt(output_power) / 0.0012163
+
+# plot_phase_shift(columns_phase_shift, frequencies, antenna_type)
+
+m_e, m_m = calculate_moment(e_field_approx, phase_shift, output_power, frequencies)
+# plot_moments(m_e, m_m, frequencies, antenna_type)
+
+# plot_output_power_e_field(frequencies, output_power, e_field_approx)
+
+def model_func(x, a, b, c):
+    return a * x**2 + b * x + c
+
+# Fit curve
+popt, pcov = curve_fit(model_func, frequencies, m_e)
+
+# Parameters fitted
+a_e, b_e, c_e = popt
+
+# Fit curve
+popt, pcov = curve_fit(model_func, frequencies, m_m)
+
+# Parameters fitted
+a_m, b_m, c_m = popt
+
+print(f"Electric Dipole Moments fitted parameters: {a_e} * freq ^ 2 + {b_e} * freq + {c_e}")
+print(f"Magnetic Dipole Moments fitted parameters: {a_m} * freq ^ 2 + {b_m} * freq + {c_m}")
+
